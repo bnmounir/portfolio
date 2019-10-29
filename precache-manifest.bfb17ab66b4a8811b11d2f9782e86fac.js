e17f8ab717bbd9464f6cc8d197ecf9f8ccffd229
Mounir Benabderrahmane <bnmounir7@gmail.com>
2019-10-29 18:30:56 -0400

    quick web app fix

---
+++ precache-manifest.bfb17ab66b4a8811b11d2f9782e86fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c81c168cc7678d9cf65aabc4489f863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d92cd0e80c44ce4a8a18",
+    "url": "/static/css/main.d34b287d.chunk.css"
+  },
+  {
+    "revision": "6b8c24b39078d3ab2e87",
+    "url": "/static/js/2.bbad4016.chunk.js"
+  },
+  {
+    "revision": "d92cd0e80c44ce4a8a18",
+    "url": "/static/js/main.298a2846.chunk.js"
+  },
+  {
+    "revision": "a5be67a9b4bd7e979ca3",
+    "url": "/static/js/runtime-main.d9a55c3c.js"
+  }
+]);
